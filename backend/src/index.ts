27c225d974641a929e43a5ace4643adc8431e34e
Harbinder Singh <harbindersingh621@gmail.com>
2024-05-20 12:22:32 +0530

    wrangler file edited and saved

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,35 @@
 import { Hono } from 'hono'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import { PrismaClient } from '@prisma/client/extension'
+
+
 
 const app = new Hono()
 
-app.get('/', (c) => {
+
+app.post('/api/v1/signup', (c) => {  // c - context 
+  const dbUrl = c.env.DATABASE_URL;
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL
+  }).$extends(withAccelerate());
+ 
+  const user = prisma.user.
+  return c.text('hello')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Hello Hono!')
+}) 
+
+app.get('/api/v1/blog/:id', (c) => {
   return c.text('Hello Hono!')
 })
 
